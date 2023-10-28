00a866b46d2200867f7e872e486890fb649d2985
Peter Wainaina <80960028+wainaina-peter@users.noreply.github.com>
2023-10-28 21:51:01 +0300

    Update streamlit_app.py

--- Deployment/streamlit_app.py
+++ Deployment/streamlit_app.py
@@ -3,21 +3,23 @@ import numpy as np
 import streamlit as st
 import os
 import string
+
 model = pickle.load(open('LRmodel.pkl', 'rb'))
 
 def main():
     st.title('Twitter(X) Sentiments Analyzer')
 
-    #input variables
+    # Input variables
     text = st.text_input('Input the tweet here (Paste it)')
     makeprediction = ""
 
-
-    #prediction code
+    # Prediction code
     if st.button('Classify Tweet'):
-        makeprediction = model.predict([text])
-        output = round(makeprediction[0],2)
+        input_data = np.array([text])
+        reshaped_data = input_data.reshape(1, -1)
+        makeprediction = model.predict(reshaped_data)
+        output = round(makeprediction[0], 2)
         st.success('The tweet is classified as {}'.format(output))
 
-if __name__=="__main__":
-    main()        
+if __name__ == "__main__":
+    main()
