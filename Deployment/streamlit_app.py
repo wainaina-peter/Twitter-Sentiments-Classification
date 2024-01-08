283d7b16cc8f50c3e3d64da67ec212a1e9f59633
Peter Wainaina <80960028+wainaina-peter@users.noreply.github.com>
2024-01-08 14:41:05 +0300

    Update streamlit_app.py

--- Deployment/streamlit_app.py
+++ Deployment/streamlit_app.py
@@ -1,25 +1,38 @@
 import pickle
 import numpy as np
+import pandas as pd
 import streamlit as st
-import os
 import string
 
-model = pickle.load(open('LRmodel.pkl', 'rb'))
+# Load the model
+with open('LRmodel.pkl', 'rb') as model_file:
+    classifier_LR = pickle.load(model_file)
+
+# Preprocess text
+def preprocess_text(text):
+    text = text.lower()  # Convert to lowercase
+    text = text.translate(str.maketrans('', '', string.punctuation))  # Remove punctuation
+    return text
+
+# Prediction function
+def predict_sentiment(text):
+    input_data = np.array([text])
+    reshaped_data = input_data.reshape(1, -1)
+    prediction = classifier_LR.predict(reshaped_data)
+    return prediction[0]
 
 def main():
     st.title('Twitter(X) Sentiments Analyzer')
 
     # Input variables
     text = st.text_input('Input the tweet here (Paste it)')
-    makeprediction = ""
+    make_prediction = ""
 
     # Prediction code
     if st.button('Classify Tweet'):
-        input_data = np.array([text])
-        reshaped_data = input_data.reshape(1, -1)
-        makeprediction = model.predict(reshaped_data)
-        output = round(makeprediction[0], 2)
-        st.success('The tweet is classified as {}'.format(output))
+        make_prediction = predict_sentiment(preprocess_text(text))
+        sentiment = "Positive" if make_prediction == 1 else "Negative"
+        st.success('The tweet is classified as {}'.format(sentiment))
 
 if __name__ == "__main__":
     main()
